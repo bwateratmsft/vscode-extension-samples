981b71b1f4c53f8fb719f5d0198a8d888e84c5ba
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2024-11-15 13:50:32 -0500

    Add a large markdown deferral

--- chat-sample/src/simple.ts
+++ chat-sample/src/simple.ts
@@ -41,6 +41,8 @@ export function registerSimpleParticipant(context: vscode.ExtensionContext) {
                 title: vscode.l10n.t('Use Cat Names in Editor')
             });
 
+            stream.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
             logger.logUsage('request', { kind: 'randomTeach' });
             return { metadata: { command: 'randomTeach' } };
         } else if (request.command === 'play') {
